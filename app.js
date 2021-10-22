0123269f028fe36a91a4833f6a3739473243f565
EatmyAspirin <ritvikashesh2000@gmail.com>
2021-10-23 03:04:05 +0530

    making-heroku-compliant

--- app.js
+++ app.js
@@ -160,6 +160,6 @@ app.get('/logout', function(req, res){
   });
 
 
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server started");
 });
